e53968c1e65e4dd12d915e7cceaf4d9a5145fd28
Werawad Runagjaroon <lennon@Werawads-MacBook-Air.local>
2017-02-22 13:18:27 +0700

    remove test code for demo TDD

--- app/src/main/java/com/example/lennon/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/lennon/currencyconverter/MainActivity.java
@@ -11,48 +11,12 @@ import com.example.lennon.currencyconverter.exchangerate.ExchangeRateServiceImpl
 
 public class MainActivity extends AppCompatActivity {
 
-    private CurrencyConverter currencyConverter;
-
-    private EditText amountEdt;
-    private TextView resultTxv;
-    private Button convertBtn;
-    private EditText currencyTxt;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        init();
-        onclickConvert();
-    }
-
-    private void onclickConvert() {
-        convertBtn.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                currencyConverter = new CurrencyConverter(new ExchangeRateServiceImpl());
-                Integer amount = Integer.valueOf(amountEdt.getText().toString());
-                String currency = currencyTxt.getText().toString();
-                getConvertCurrency(amount, currency);
-            }
-        });
-    }
-
-    private void getConvertCurrency(Integer amount, String currency) {
-        try {
-            int result = currencyConverter.converterCurrency(amount, currency);
-            resultTxv.setText(String.valueOf(result));
-        }catch (Exception e){
-            resultTxv.setText("Can not convert!!!");
-        }
-    }
-
-    protected void init() {
-        amountEdt = (EditText) findViewById(R.id.amountText);
-        resultTxv = (TextView) findViewById(R.id.resultTextView);
-        convertBtn = (Button) findViewById(R.id.convertButton);
-        currencyTxt = (EditText) findViewById(R.id.currencyTxt);
     }
 
 
-}// end class
+}
--- app/src/main/java/com/example/lennon/currencyconverter/converter/CurrencyConverter.java
+++ app/src/main/java/com/example/lennon/currencyconverter/converter/CurrencyConverter.java
@@ -10,44 +10,5 @@ import com.example.lennon.currencyconverter.exchangerate.ExchangeRateService;
 @SuppressWarnings("Since15")
 public class CurrencyConverter {
 
-    private ExchangeRateService exchangeRateService;
 
-    public CurrencyConverter(ExchangeRateService exchangeRateService) {
-        this.exchangeRateService = exchangeRateService;
-    }
-
-    /**
-     * Converter Currency
-     *
-     * @param amount
-     * @param currency
-     * @return
-     */
-    public int converterCurrency(int amount, String currency) {
-        validateAmount(amount);
-        validateCurrency(currency);
-        return multiply(amount, exchangeRateService.getRate(currency));
-    }
-
-    private int multiply(int amount, int rate){
-        try {
-            return Math.multiplyExact(amount, rate);
-        } catch (Exception e) {
-            throw e;
-        }
-    }
-
-    private void validateAmount(int amount) {
-        if (amount < 0) {
-            throw new IllegalArgumentException("Amount can't be negative.");
-        }
-    }
-
-    private int validateCurrency(String currency) {
-        try {
-            return exchangeRateService.getRate(currency);
-        } catch (Exception e) {
-            throw new IllegalArgumentException("Not found currency");
-        }
-    }
 }
